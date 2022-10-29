093ebd8a518ed16975f22ebd14158eb6f2e4a2f7
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-10-30 02:01:37 +0800

    [QOL] / Sorted transactions via descending transaction date

--- src/Controllers/transaction.controller.js
+++ src/Controllers/transaction.controller.js
@@ -3,7 +3,11 @@ import { connect, client, disconnect } from "../Utils/prismaHandler.js";
 const getAllTransaction = async (req, res) => {
     await connect()
 
-    const transactions = await client.Transaction.findMany()
+    const transactions = await client.Transaction.findMany({
+        orderBy: {
+            transaction_date: "desc"
+        }
+    })
 
     res.status(200).json({
         transactions
