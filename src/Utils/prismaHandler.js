41b0a1bf03bcc18118be039854dc04f72dd3979a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-11-12 19:40:27 +0800

    [UPDATE] / Add logging capability

--- src/Utils/prismaHandler.js
+++ src/Utils/prismaHandler.js
@@ -1,6 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 
-const client = new PrismaClient()
+const client = new PrismaClient({
+    log: [
+        { level: 'warn', emit: 'event' },
+        { level: 'info', emit: 'event' },
+        { level: 'error', emit: 'event' },
+    ],
+    errorFormat: 'pretty'
+})
+
+client.$on('warn', (e) => {
+    console.log(e)
+})
+
+client.$on('info', (e) => {
+    console.log(e)
+})
+
+client.$on('error', (e) => {
+    console.log(e)
+})
 
 const connect = async () => {
     await client.$connect()
